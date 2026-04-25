9d2f0f2ff4fb02ff05fd007764cfb050c252b7fc
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2026-04-25 21:56:13 +0600

    Create Question.cpp

---
+++ Lab-0/task3/Question.cpp
@@ -0,0 +1,17 @@
+Propose an algorithm which will print co-ordinate of a peak in a 2D matrix. A peak in a matrix is a
+location with the property that its four neighbors (north, south, east, and west) have value less than
+or equal to the value of the peak. If the following matrix is given as input you need to output (1,1).
+
+0 1 0
+1 2 1
+0 1 0
+
+You don’t need to do anything if there is no element left to compare. For the following matrix, answer
+is (0,1) or (2,1) since there can be multiple correct answers.
+0 1 0
+0 0 0
+0 0 0
+
+Your algorithm should have lower or equal to O(row ∗ log(column)) time complexity. For
+better marks implement your algorithm in python and test with different test cases.
+Submit your implementation with your report.
