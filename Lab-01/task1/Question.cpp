97a053c76e01bb6b2c8a3d1f3c2728cfc1f3105f
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2026-05-07 15:51:56 +0600

    Create Question.cpp

---
+++ Lab-01/task1/Question.cpp
@@ -0,0 +1,9 @@
+Given an array arr[] of integers, the task is to find the Next Greater Element for each element of the
+array in order of their appearance in the array. You need to give a solution with O(n) time and O(n)
+space complexity.
+Note: The Next Greater Element for an element x is the first greater element on the right side of x
+in the array. Elements for which no greater element exist, consider the next greater element as -1.
+Input: arr[] = [1, 3, 2, 4]
+Output: [3, 4, 4, -1]
+Input: arr[] = [6, 8, 0, 1, 3]
+Output: [8, -1, 1, 3, -1]
