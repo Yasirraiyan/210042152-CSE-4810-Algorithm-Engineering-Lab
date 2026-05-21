69240fdb88b2a82c2b8d1be8d4a6650b4fcf8f6d
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2026-05-21 15:55:42 +0600

    Create Question.cpp

---
+++ Lab-02/task-01/Question.cpp
@@ -0,0 +1,7 @@
+Let’s say you have a BST and now I want you to find all the available nodes in the given range.
+First convert the list given in first line of the input to a BST using insert function given in the helping
+code. Then try to find all the nodes available in the given range in the second line.
+Input:
+ar = [11, 2, 29, 1, 7, 15, 40, 35]
+range = (12,35)
+output: 15, 29, 35
